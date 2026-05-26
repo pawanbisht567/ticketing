8fca3642540e633544f671baf8a7ef45aba69055
Pawan Bisht <138034621+pawanbisht567@users.noreply.github.com>
2026-05-27 00:30:23 +0530

    New changes for the k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.json())
 
-app.listen(3000, () =>{
-  console.log('Server is running on port 3000!')
+app.listen(3000, () => {
+  console.log('Server running on port 3000!!!!')
 })
