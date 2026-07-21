d5e05128b96eb16a8759eff2efbe0e9b8e2c39fe
Pawan Bisht <138034621+pawanbisht567@users.noreply.github.com>
2026-07-21 22:37:22 +0530

    Adding new things

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieSession from 'cookie-session';
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
 import { signinRouter } from './routes/signin';
@@ -10,7 +11,12 @@ import mongoose from 'mongoose';
 
 const app = express()
 
+app.set('trust proxy', true) // trust first proxy
 app.use(express.json())
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}))
 app.use(requestIdHandler)
 app.use(currentUserRouter)
 app.use(signinRouter)
