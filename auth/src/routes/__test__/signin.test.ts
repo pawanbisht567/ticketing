e2283919c95e57e8aed3a062c2dfe9e0249159f5
Pawan Bisht <138034621+pawanbisht567@users.noreply.github.com>
2026-09-05 12:08:43 +0530

    New test cases added

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,75 @@
+import request from "supertest"
+import { app } from '../../app'
+import { it, expect } from '@jest/globals'
+
+it('fails when a email that does not exist is supplied', async() => {
+    return request(app)
+    .post('/api/users/signin')
+    .send({
+        "email": "nonexistent@gmail.com",
+        "password": "password123"
+    })
+    .expect(400)
+})
+
+it('pass when correct credentials are supplied', async() => {
+    // First, sign up a user
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "test@gmail.com",
+        "age": 34,
+        "password": "password123"
+    })
+    .expect(201)
+
+    // Then, sign in with the same credentials
+    return request(app)
+    .post('/api/users/signin')
+    .send({
+        "email": "test@gmail.com",
+        "password": "password123"
+    })
+    .expect(200)
+})
+
+it('fails when an incorrect password is supplied', async() => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "test@gmail.com",
+        "password": "password123"
+    })
+    .expect(201)
+
+    // Then, sign in with the same email but wrong password
+    return request(app)
+    .post('/api/users/signin')
+    .send({
+        "email": "test@gmail.com",
+        "password": "password1234"
+    })
+    .expect(400)
+})
+
+it('sets a cookie after successful signin', async() => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "test@gmail.com",
+        "age": 34,
+        "password": "password123"
+    })
+    .expect(201)
+
+    // Then, sign in with the same email but wrong password
+    let signinRequest = await request(app)
+    .post('/api/users/signin')
+    .send({
+        "email": "test@gmail.com",
+        "password": "password123"
+    })
+    .expect(200)
+    console.log(signinRequest)
+    expect(signinRequest.get('Set-Cookie')).toBeDefined()
+})
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest"
 import { app } from '../../app'
-import { it } from '@jest/globals'
+import { it, expect } from '@jest/globals'
 
 it('return 201 for successful signup', async() => {
     return request(app)
@@ -11,4 +11,79 @@ it('return 201 for successful signup', async() => {
         "password": "esfsewdeeeww"
     })
     .expect(201)
+})
+
+it('returns 400 with an invalid email', async() => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "adadwe@",
+        "age": 34,
+        "password": "esfsewdeeeww"
+    })
+    .expect(400)
+})
+
+it('returns 400 with an invalid password', async() => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "adadwe@gmail.com",
+        "age": 34,
+        "password": "esfs"
+    })
+    .expect(400)
+})
+
+it('returns 400 with an invalid password', async() => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "adadwe@gmail.com",
+        "age": 34,
+        "password": "esfsdwaaaaaaaaaaaaaaaaaawdawdaw"
+    })
+    .expect(400)
+})
+
+it('returns 400 with an invalid password', async() => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "",
+        "age": 34,
+        "password": ""
+    })
+    .expect(400)
+})
+
+it('return 400 for duplicate email', async() => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "awa1a122@gmail.com",
+        "age": 34,
+        "password": "esfsewdeeeww"
+    }).expect(201)
+
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "awa1a122@gmail.com",
+        "age": 34,
+        "password": "esfsewdeeeww"
+    }).expect(400)
+})
+
+it('sets a cookie after successful signup', async() => {
+    const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+        "email": "awa1a122@gmail.com",
+        "age": 34,
+        "password": "esfsewdeeeww"
+    })
+    .expect(201)
+
+    expect(response.get('Set-Cookie')).toBeDefined()
 })
